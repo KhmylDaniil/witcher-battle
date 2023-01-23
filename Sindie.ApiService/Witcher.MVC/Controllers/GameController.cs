5ac65edbe7f98e1603e3fc0255e761e2d92bd322
DaniilKhmyl <khmyldaniil@gmail.com>
2023-01-23 22:30:59 +0300

    eod

--- Sindie.ApiService/Witcher.MVC/Controllers/GameController.cs
+++ Sindie.ApiService/Witcher.MVC/Controllers/GameController.cs
@@ -1,5 +1,6 @@
 ﻿using MediatR;
 using Microsoft.AspNetCore.Mvc;
+using Sindie.ApiService.Core.Contracts.GameRequests.CreateGame;
 using Sindie.ApiService.Core.Contracts.GameRequests.GetGame;
 
 namespace Witcher.MVC.Controllers
@@ -14,25 +15,17 @@ namespace Witcher.MVC.Controllers
 		}
 
 		// GET: GameController
-		public async Task<IActionResult> Index(string? name, string? userName, string? username2, CancellationToken cancellationToken)
+		public async Task<IActionResult> Index(string name, string description, string authorName, CancellationToken cancellationToken)
 		{
-			var query = new GetGameQuery() { Name = name, AuthorName = userName };
+			var query = new GetGameQuery() { Name = name, Description = description, AuthorName = authorName};
 			
 			var response = await _mediator.Send(query, cancellationToken);
 
-			if (string.IsNullOrEmpty(username2))
-				return View(response.GamesList);
-
-			var query2 = new GetGameQuery() { Name = name, AuthorName = username2 };
-
-			var response2 = await _mediator.Send(query2, cancellationToken);
-
-			var totalResult = response.GamesList.Union(response2.GamesList);
-			return View(totalResult);	
+			return View(response.GamesList);
 		}
 
 		// GET: GameController/Details/5
-		public ActionResult Details(int id)
+		public ActionResult Enter(Guid id)
 		{
 			return View();
 		}
@@ -46,10 +39,11 @@ namespace Witcher.MVC.Controllers
 		// POST: GameController/Create
 		[HttpPost]
 		[ValidateAntiForgeryToken]
-		public ActionResult Create(IFormCollection collection)
+		public async Task<IActionResult> Create(CreateGameCommand command, CancellationToken cancellationToken)
 		{
 			try
 			{
+				await _mediator.Send(command ?? throw new ArgumentNullException(nameof(command)), cancellationToken);
 				return RedirectToAction(nameof(Index));
 			}
 			catch
--- Sindie.ApiService/Witcher.MVC/Program.cs
+++ Sindie.ApiService/Witcher.MVC/Program.cs
@@ -33,7 +33,7 @@ app.MapControllerRoute(
 	name: "default",
 	pattern: "{controller=Home}/{action=Index}");
 
-app.MapControllerRoute(name: "Game", pattern: "{controller}/{action}/{name?}/{userName?}/{userName2?}");
+//app.MapControllerRoute(name: "Game", pattern: "{controller}/{action}/{name?}/{userName?}/{userName2?}");
 
 //app.MapControllerRoute(
 //	name: "Game",
