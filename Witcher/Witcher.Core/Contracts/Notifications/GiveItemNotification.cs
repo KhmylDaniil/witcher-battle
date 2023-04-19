b8d28bc9e96db4c1ccdeafb696adb8fa6bafe11e
DaniilKhmyl <khmyldaniil@gmail.com>
2023-04-19 10:06:37 +0300

    underConstruction

---
+++ Witcher/Witcher.Core/Contracts/Notifications/GiveItemNotification.cs
@@ -0,0 +1,7 @@
+﻿using MediatR;
+using Witcher.Core.Entities;
+
+namespace Witcher.Core.Contracts.Notifications
+{
+	public record GiveItemNotification(Item Item, Character Giver, Character Receiver) : INotification;
+}
--- Witcher/Witcher.Core/Requests/BagRequests/GiveItemToAnotherCharacterHandler.cs
+++ Witcher/Witcher.Core/Requests/BagRequests/GiveItemToAnotherCharacterHandler.cs
@@ -1,42 +1,49 @@
 ﻿using MediatR;
 using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using Witcher.Core.Abstractions;
 using Witcher.Core.Contracts.BagRequests;
+using Witcher.Core.Contracts.Notifications;
 using Witcher.Core.Entities;
 using Witcher.Core.Exceptions.EntityExceptions;
 using Witcher.Core.Exceptions.RequestExceptions;
 
 namespace Witcher.Core.Requests.BagRequests
 {
-	public class RemoveItemFromBagHandler : BaseHandler<RemoveItemFromBagCommand, Unit>
+	public class GiveItemToAnotherCharacterHandler : BaseHandler<GiveItemToAnotherCharacterCommand, GiveItemNotification>
 	{
-		public RemoveItemFromBagHandler(IAppDbContext appDbContext, IAuthorizationService authorizationService) : base(appDbContext, authorizationService)
+		public GiveItemToAnotherCharacterHandler(IAppDbContext appDbContext, IAuthorizationService authorizationService)
+			: base(appDbContext, authorizationService)
 		{
 		}
 
-		public async override Task<Unit> Handle(RemoveItemFromBagCommand request, CancellationToken cancellationToken)
+		public async override Task<GiveItemNotification> Handle(GiveItemToAnotherCharacterCommand request, CancellationToken cancellationToken)
 		{
 			var game = await _authorizationService.CharacterOwnerFilter(_appDbContext.Games, request.CharacterId)
-				.Include(g => g.Characters.Where(c => c.Id == request.CharacterId))
+				.Include(g => g.Characters)
 					.ThenInclude(c => c.Bag)
+				.Include(g => g.Characters.Where(c => c.Id == request.CharacterId))
+					.ThenInclude(c => c.EquippedWeapons)
 				.FirstOrDefaultAsync(cancellationToken)
 					?? throw new NoAccessToEntityException<Game>();
 
 			var character = game.Characters.FirstOrDefault(c => c.Id == request.CharacterId)
 				?? throw new EntityNotFoundException<Character>(request.CharacterId);
 
+			var targetCharacter = game.Characters.FirstOrDefault(c => c.Id == request.TargetCharacterId)
+				?? throw new EntityNotFoundException<Character>(request.TargetCharacterId);
+
 			var bag = character.Bag ?? throw new EntityNotIncludedException<Character>(nameof(Bag));
 
 			var item = bag.Items.FirstOrDefault(x => x.Id == request.ItemId)
 				?? throw new EntityNotFoundException<ItemTemplate>(request.ItemId);
-			
-			bag.RemoveItems(item, request.Quantity);
 
-			await _appDbContext.SaveChangesAsync(cancellationToken);
-			return Unit.Value;
+			return new GiveItemNotification(item, character, targetCharacter);
 		}
 	}
 }
