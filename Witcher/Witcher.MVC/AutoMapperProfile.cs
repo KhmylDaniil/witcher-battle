7e845263edecbd8115cbe50204cf566eed7c4911
DaniilKhmyl <khmyldaniil@gmail.com>
2023-06-02 10:12:13 +0300

    AddedArmorTemplateController

--- Witcher/Witcher.MVC/AutoMapperProfile.cs
+++ Witcher/Witcher.MVC/AutoMapperProfile.cs
@@ -1,8 +1,10 @@
 ﻿using AutoMapper;
+using Witcher.Core.Contracts.ArmorTemplateRequests;
 using Witcher.Core.Contracts.BattleRequests;
 using Witcher.Core.Contracts.CreatureTemplateRequests;
 using Witcher.Core.Contracts.RunBattleRequests;
 using Witcher.Core.Contracts.UserGameRequests;
+using Witcher.MVC.ViewModels.ArmorTemplate;
 using Witcher.MVC.ViewModels.Battle;
 using Witcher.MVC.ViewModels.CreatureTemplate;
 using Witcher.MVC.ViewModels.Game;
@@ -20,6 +22,8 @@ namespace Witcher.MVC
 
 			CreateMap<CreateCreatureTemplateCommand, CreateCreatureTemplateCommandViewModel>();
 			CreateMap<ChangeCreatureTemplateCommand, ChangeCreatureTemplateCommandViewModel>();
+
+			CreateMap<CreateArmorTemplateCommand, CreateArmorTemplateViewModel>();
 
 			CreateMap<CreateCreatureCommand, CreateCreatureCommandViewModel>();
 			CreateMap<AddCharacterToBattleCommand, AddCharacterToBattleViewModel>();
---
+++ Witcher/Witcher.MVC/ViewModels/ArmorTemplate/CreateArmorTemplateViewModel.cs
@@ -0,0 +1,12 @@
+﻿using Witcher.Core.Contracts.ArmorTemplateRequests;
+
+namespace Witcher.MVC.ViewModels.ArmorTemplate
+{
+	public class CreateArmorTemplateViewModel : CreateArmorTemplateCommand
+	{
+		/// <summary>
+		/// Данные шаблонов тела
+		/// </summary>
+		public Dictionary<Guid, string> BodyTemplatesDictionary { get; set; }
+	}
+}
